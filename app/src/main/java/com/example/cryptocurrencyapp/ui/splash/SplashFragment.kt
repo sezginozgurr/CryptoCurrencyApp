0ef9cae3da2e70e061fd5a783d4a35e886b355c8
Özgür Can <sezginzgurr@gmail.com>
2021-11-25 10:37:03 +0300

    Added SplashFragment

---
+++ app/src/main/java/com/example/cryptocurrencyapp/ui/splash/SplashFragment.kt
@@ -0,0 +1,13 @@
+package com.example.cryptocurrencyapp.ui.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cryptocurrencyapp.R
+
+
+class SplashFragment : Fragment() {
+
+}
