660a0d022118b0495e02c55e19233a1bef99896b
Özgür Can <sezginzgurr@gmail.com>
2021-11-25 10:33:41 +0300

    Added servise end point

---
+++ app/src/main/java/com/example/cryptocurrencyapp/data/service/ApiService.kt
@@ -0,0 +1,29 @@
+package com.example.cryptocurrencyapp.data.service
+
+import com.example.cryptocurrencyapp.data.model.CoinDetailResponse
+import com.example.cryptocurrencyapp.data.model.CoinListResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("coins/markets")
+    suspend fun getCoinList(
+        @Query("vs_currency") vs_currency: String = "try",
+        @Query("order") order: String = "market_cap_desc",
+        @Query("per_page") per_page: Int = 50,
+        @Query("page") page: Int = 1,
+        @Query("sparkline") sparkline: Boolean = false
+    ): List<CoinListResponse>
+
+    @GET("coins/{id}")
+    suspend fun getCoinDetail(
+        @Path("id") id: String,
+        @Query("localization") localization: Boolean = false,
+        @Query("tickers") tickers: Boolean = false,
+        @Query("market_data") market_data: Boolean = true,
+        @Query("community_data") community_data: Boolean = false,
+        @Query("developer_data") developer_data: Boolean = false,
+    ): CoinDetailResponse
+}
