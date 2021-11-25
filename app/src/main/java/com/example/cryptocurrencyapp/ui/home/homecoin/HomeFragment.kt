8eba1f06bb32dd4a604908111df8239c800ca7b2
Özgür Can <sezginzgurr@gmail.com>
2021-11-25 10:34:30 +0300

    added HomeFragment

---
+++ app/src/main/java/com/example/cryptocurrencyapp/ui/home/homecoin/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.example.cryptocurrencyapp.ui.home.homecoin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cryptocurrencyapp.R
+
+class HomeFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/example/cryptocurrencyapp/ui/home/homecoin/HomeViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.cryptocurrencyapp.ui.home.homecoin
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.cryptocurrencyapp.data.repository.CoinRepository
+import com.example.cryptocurrencyapp.util.Resource
+import kotlinx.coroutines.Dispatchers
+
+class HomeViewModel(private val repository: CoinRepository) : ViewModel() {
+
+    fun getCoinList() = liveData(Dispatchers.IO) {
+        emit(Resource.loading(null))
+        try {
+            emit(Resource.success(repository.getCoinList()))
+        } catch (exception: Exception) {
+            emit(Resource.error(null, exception.message ?: "Failure!"))
+        }
+    }
+}
