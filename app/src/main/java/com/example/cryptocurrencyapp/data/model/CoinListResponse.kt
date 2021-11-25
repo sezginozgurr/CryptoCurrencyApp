aafb1670fa562bcaf6c296ac5db9c0d1774a5e5e
Özgür Can <sezginzgurr@gmail.com>
2021-11-25 10:35:35 +0300

    added Model in crypto

---
+++ app/src/main/java/com/example/cryptocurrencyapp/data/model/CoinListResponse.kt
@@ -0,0 +1,15 @@
+package com.example.cryptocurrencyapp.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class CoinListResponse(
+    val current_price: Double?,
+    val id: String? = "",
+    val image: String? = "",
+    val name: String? = "",
+    val price_change_24h: Double? = 0.0
+) : Parcelable {
+    constructor() : this(0.0)
+}
