86be19a38195abda1b04b1e01382aa5631716f82
Özgür Can <sezginzgurr@gmail.com>
2021-11-25 10:36:29 +0300

    Added MainFragment

---
+++ app/src/main/java/com/example/cryptocurrencyapp/ui/home/MainFragment.kt
@@ -0,0 +1,7 @@
+package com.example.cryptocurrencyapp.ui.home
+
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/example/cryptocurrencyapp/ui/home/MainViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.cryptocurrencyapp.ui.home
+
+class MainViewModel {
+}
