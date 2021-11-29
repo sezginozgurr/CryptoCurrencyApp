82785590526b624f13081c27ff38e7c4984e28e2
Özgür Can <sezginzgurr@gmail.com>
2021-11-29 23:07:10 +0300

    Added modules

--- app/src/main/java/com/example/cryptocurrencyapp/di/AppModule.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/di/AppModule.kt
@@ -2,12 +2,14 @@ package com.example.cryptocurrencyapp.di
 
 import com.example.cryptocurrencyapp.data.repository.CoinRepository
 import com.example.cryptocurrencyapp.ui.home.HomeViewModel
+import com.example.cryptocurrencyapp.ui.home.coindetail.CoinDetailViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
     val appModule = module {
 
         viewModel { HomeViewModel(get()) }
+        viewModel { CoinDetailViewModel(get()) }
 
         single { CoinRepository(get()) }
     }
