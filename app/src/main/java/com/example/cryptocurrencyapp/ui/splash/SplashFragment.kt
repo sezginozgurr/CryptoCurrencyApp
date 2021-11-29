5385dd9f9e20fad58c8318f5c677c3ed80be4f4e
Özgür Can <sezginzgurr@gmail.com>
2021-11-29 23:08:39 +0300

    Added countdown timer

--- app/src/main/java/com/example/cryptocurrencyapp/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/ui/splash/SplashFragment.kt
@@ -1,13 +1,52 @@
 package com.example.cryptocurrencyapp.ui.splash
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.cryptocurrencyapp.R
-
+import com.example.cryptocurrencyapp.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var countDownTimer: CountDownTimer
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.splashAnimation.playAnimation()
+
+        countDownTimer = object : CountDownTimer(3000, 1000) {
+            override fun onFinish() {
+                findNavController().popBackStack()
+                    findNavController().navigate(R.id.homeFragment)
+            }
+
+            override fun onTick(millisUntilFinished: Long) {}
+        }.start()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        countDownTimer.cancel()
+    }
 
 }
